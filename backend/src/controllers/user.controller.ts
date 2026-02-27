c004ef4fb62b93b4ca53c964656010a31c4f8550
sanemi07 <arjuntripathi1724@gmail.com>
2026-02-27 10:46:40 +0530

    signinController

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -35,5 +35,28 @@ export const signUpUser=async(c:Context)=>{
      }
 }
 export const signInUser=async(c:Context)=>{
-    
+    try {
+         const prisma=getPrisma(c.env.DATABASE_URL)
+        const body=await c.req.json()
+        const user=await prisma.user.findFirst({where:{
+            email:body.email
+        }})
+        if(!user){
+            return c.json({msg:"user email not  exist"},500)
+        }
+        const isPassword=await bcrypt.compare(body.password,user.password)
+        if(!isPassword){
+             return c.json({msg:"upasword wrong"},500)
+        }
+         const token=sign({id:user.id},c.env.JWT_SECRET)
+        if(!token){
+             return c.json({msg:"token not created"},500)
+        }
+          return c.json({jwt:token})
+
+        
+
+    } catch (error) {
+        return c.json({msg:"user signin error",error},500)
+    }
 }
